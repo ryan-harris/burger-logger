49f908268cc0b0d8440d9e65e103c714f7d9a7f0
Ryan Harris <harris.ucla@gmail.com>
2020-04-15 17:57:18 -0700

    Create connection, updated SQL files

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Bacon Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Deluxe Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Turkey Burger", false);
