d40d4ed52dca2fc1af3a050a353eca3e1ee7de08
Ryan Harris <harris.ucla@gmail.com>
2020-04-16 15:06:40 -0700

    add api routes for creating/updating burgers

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -4,8 +4,34 @@ const burger = require("../models/burger");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  burger.getBurgers((burgers) => {
+  burger.selectAll((burgers) => {
     res.render("index", { burgers });
+  });
+});
+
+router.post("/api/burgers", (req, res) => {
+  const newBurger = {
+    burger_name: req.body.burger_name,
+    devoured: false
+  };
+
+  burger.insertOne(newBurger, (result) => {
+    if (result.affectedRows === 0) {
+      return res.sendStatus(404);
+    }
+    res.sendStatus(200);
+  });
+});
+
+router.put("/api/burgers/:id", (req, res) => {
+  const condition = { id: req.params.id };
+  const update = { devoured: req.body.devoured };
+
+  burger.updateOne(update, condition, (result) => {
+    if (result.affectedRows === 0) {
+      return res.sendStatus(404);
+    }
+    res.sendStatus(200);
   });
 });
 
