a1e861469e260296092bc56d7bc60fd52d4f0ccf
Ryan Harris <harris.ucla@gmail.com>
2020-04-16 15:06:12 -0700

    update ORM to insert and update, hook model to new orm functions

--- config/orm.js
+++ config/orm.js
@@ -1,8 +1,9 @@
 const connection = require("./connection.js");
 
 const orm = {
-  selectAll: (table, cb) => {
-    connection.query("SELECT * FROM ??", [table], (err, result) => {
+  selectAll: (tableName, cb) => {
+    const sql = "SELECT * FROM ??";
+    connection.query(sql, [tableName], (err, result) => {
       if (err) {
         throw err;
       }
@@ -10,9 +11,25 @@ const orm = {
     });
   },
 
-  insertOne: () => {},
+  insertOne: (tableName, newData, cb) => {
+    const sql = "INSERT INTO ?? SET ?";
+    connection.query(sql, [tableName, newData], (err, result) => {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  },
 
-  updateOne: () => {}
+  updateOne: (tableName, updateData, condition, cb) => {
+    const sql = "UPDATE ?? SET ? WHERE ?";
+    connection.query(sql, [tableName, updateData, condition], (err, result) => {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  }
 };
 
 module.exports = orm;
--- models/burger.js
+++ models/burger.js
@@ -1,8 +1,16 @@
 const orm = require("../config/orm");
 
 const burger = {
-  getBurgers: (cb) => {
+  selectAll: (cb) => {
     orm.selectAll("burgers", cb);
+  },
+
+  insertOne: (burgerData, cb) => {
+    orm.insertOne("burgers", burgerData, cb);
+  },
+
+  updateOne: (burgerData, criteria, cb) => {
+    orm.updateOne("burgers", burgerData, criteria, cb);
   }
 };
 
